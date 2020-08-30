d7e9144dbfec8f9d7980fe11a7b5b01013ae962d
Paul Grau <paul@blocko.io>
2020-08-30 23:32:08 +0900

    Remove the crap

--- src/components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Icon } from './Icon.vue';
-export { default as SuccessIcon } from './SuccessIcon.vue';
-export { default as LoadingIndicator } from './LoadingIndicator.vue';
-export { default as HeaderLogo } from './HeaderLogo.vue';
--- src/components/icons/types.ts
+++
@@ -1,9 +0,0 @@
-export const iconNames = [
-  'arrow',
-  'checkmark',
-  'danger',
-  'logo',
-  'net',
-] as const;
-
-export type IconName = typeof iconNames[number];
